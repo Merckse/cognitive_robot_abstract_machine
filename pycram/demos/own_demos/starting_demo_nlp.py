3f5818f13b4b86a8940f89e86f416b174ebbb3f6
hanna135 <majazuchow@gmail.com>
2026-01-14 17:06:09 +0100

    changes on project laptop

--- pycram/demos/own_demos/starting_demo_nlp.py
+++ pycram/demos/own_demos/starting_demo_nlp.py
@@ -1,7 +1,8 @@
 from typing import Any
 
 import rclpy
-#import gpsr_01
+
+# import gpsr_01
 from pycram.external_interfaces.nlp_interface import NlpInterface
 import subprocess
 import socket
@@ -11,26 +12,23 @@ import time
 import requests
 
 
-
 class NlpInterfaceStartingDemo(NlpInterface):
 
     def __init__(self):
         super().__init__()
 
-    def filter_response(self, response : list[Any], challenge : str):
+    def filter_response(self, response: list[Any], challenge: str):
         # TODO: role einbinden, damit challenge ausgegeben wird
         print(response)
-        return response[2][0][1] # should return value from entity_elem
+        return response[2][0][1]  # should return value from entity_elem
+
 
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('0.0.0.0', port)) == 0
+        return s.connect_ex(("0.0.0.0", port)) == 0
 
-def wait_for_rasa(
-    url="http://localhost:5005/status",
-    timeout=120,
-    interval=1
-):
+
+def wait_for_rasa(url="http://localhost:5005/status", timeout=120, interval=1):
     start = time.time()
     while time.time() - start < timeout:
         try:
@@ -44,6 +42,7 @@ def wait_for_rasa(
 
     raise TimeoutError("Rasa did not start in time")
 
+
 def wait_for_whisper(proc, keyword="started", timeout=60):
     start = time.time()
     while time.time() - start < timeout:
@@ -54,7 +53,7 @@ def wait_for_whisper(proc, keyword="started", timeout=60):
     raise TimeoutError("Whisper did not start")
 
 
-def end_process(procs : list[subprocess.Popen]):
+def end_process(procs: list[subprocess.Popen]):
     for proc in procs:
         try:
             parent = psutil.Process(proc.pid)
@@ -83,31 +82,36 @@ def main():
     """
     nlp = NlpInterfaceStartingDemo()
 
-
     # DEBUG
     # nlp.last_output = ["Start", "", ["", "GPSR"]]
 
     while True:
+
         # starts NLP Pipeline for demo start
-        proc = subprocess.Popen('bash -i -c "nlp_rasa_demo_start"', shell=True)
+        proc = subprocess.Popen('bash -i -c "nlp_rasa_start"', shell=True)
         wait_for_rasa()
 
-        proc2 = subprocess.Popen('bash -i -c "nlp_whisper_start"', shell=True, stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT, text=True)
+        proc2 = subprocess.Popen(
+            'bash -i -c "nlp_whisper_start"',
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
         wait_for_whisper(proc2)
 
         sleep(3)
 
-        #nlp.input_confirmation_loop(4)
+        # nlp.input_confirmation_loop(4)
         nlp.start_nlp()
 
         print(nlp.last_output)
 
+        end_process([proc, proc2])
+
         # if intent wasn't understood right even after confirmation and repeating n-times
         if nlp.last_output[1] != "Start" and nlp.last_output[0] != "Change":
             raise NotImplementedError("NLP Intent should not happen.")
-
-        end_process([proc, proc2])
 
         # waits until rasa port is free again
         while is_port_in_use(5005):
@@ -119,27 +123,35 @@ def main():
 
         # starts rasa with model for challenge
         match nlp.filter_response(nlp.last_output, ""):
-            case 'Receptionist':
-                proc = subprocess.Popen('bash -i -c "nlp_rasa_receptionist_start"', shell=True) # befehle sind alias befehle
-            case 'GPSR':
+            case "Receptionist":
+                proc = subprocess.Popen(
+                    'bash -i -c "nlp_rasa_receptionist_start"', shell=True
+                )  # befehle sind alias befehle
+            case "GPSR":
                 proc = subprocess.Popen('bash -i -c "nlp_rasa_gpsr_start"', shell=True)
-                #gpsr_01.main()
-            case 'Restaurant':
-                proc = subprocess.Popen('bash -i -c "nlp_rasa_restaurant_start"', shell=True)
+            case "Restaurant":
+                proc = subprocess.Popen(
+                    'bash -i -c "nlp_rasa_restaurant_start"', shell=True
+                )
             case _:
                 NotImplemented()
         wait_for_rasa()
-        proc2 = subprocess.Popen('bash -i -c "nlp_whisper_start"', shell=True, stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT, text=True)
+        proc2 = subprocess.Popen(
+            'bash -i -c "nlp_whisper_start"',
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
         wait_for_whisper(proc2)
 
         match nlp.filter_response(nlp.last_output, ""):
-            case 'Receptionist':
+            case "Receptionist":
                 print("Insert code here.......")
-            case 'GPSR'|'GPS'|'GPS R':
+            case "GPSR" | "GPS" | "GPS R":
                 print("Insert GPSR code here.......")
                 # gpsr_01.main()
-            case 'Restaurant':
+            case "Restaurant":
                 print("Insert code here.......")
             case _:
                 print("couldn't understand your input")
@@ -152,11 +164,10 @@ def main():
 
         print("If you want to stop, interrupt now")
         number = 5
-        for i in range (5, 0):
+        while number > -1:
             print(number)
             number -= 1
             sleep(2)
-
 
 
 if __name__ == "__main__":

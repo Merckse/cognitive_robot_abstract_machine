194bb1068f8f6d8fa85bd00e47fcfea1dcc4efb7
Merckse <marc.kruse.2003@gmail.com>
2026-01-28 17:19:46 +0100

    testing

--- pycram/demos/pycram_hsrb_demo/pick_up_demo_clean.py
+++ pycram/demos/pycram_hsrb_demo/pick_up_demo_clean.py
@@ -23,15 +23,23 @@ from pycram.robot_plans import (
     MoveTorsoActionDescription,
     PickUpActionDescription,
 )
+from pycram.ros import VizMarkerPublisher
 from semantic_digital_twin.adapters.ros.world_fetcher import fetch_world_from_service
 from semantic_digital_twin.adapters.ros.world_synchronizer import (
     ModelSynchronizer,
     StateSynchronizer,
 )
+from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
 from semantic_digital_twin.robots.hsrb import HSRB
 from pycram_ros_setup import setup_ros_node
-from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix
+from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix, Vector3
 from pycram.alternative_motion_mappings import hsrb_motion_mapping
+from semantic_digital_twin.spatial_types.derivatives import DerivativeMap
+from semantic_digital_twin.world_description.connections import PrismaticConnection
+from semantic_digital_twin.world_description.degree_of_freedom import DegreeOfFreedom
+from semantic_digital_twin.world_description.geometry import Box, Scale
+from semantic_digital_twin.world_description.shape_collection import ShapeCollection
+from semantic_digital_twin.world_description.world_entity import Body
 
 logger = logging.getLogger(__name__)
 
@@ -49,6 +57,26 @@ print(perceived_objects)
 
 grasp = GraspDescription(ApproachDirection.FRONT, VerticalAlignment.NoAlignment, False)
 
+with hsrb_world.modify_world():
+    box = Body(
+        name=PrefixedName("milk"),
+        collision=ShapeCollection([Box(scale=Scale(0.2, 0.1, 0.2))]),
+    )
+    dof = DegreeOfFreedom(
+        lower_limits=DerivativeMap(data=[None, -1.0, None, None]),
+        upper_limits=DerivativeMap(data=[None, 1.0, None, None]),
+    )
+    hsrb_world.add_degree_of_freedom(dof)
+    connection = PrismaticConnection(
+        dof_id=dof.id,
+        parent=hsrb_world.root,
+        child=box,
+        axis=Vector3.Z(reference_frame=hsrb_world.root),
+        parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(
+            x=2, y=0.5, z=0.5, yaw=0
+        ),
+    )
+    hsrb_world.add_connection(connection)
 
 # plan1 = SequentialPlan(
 #     context,
@@ -69,7 +97,7 @@ grasp = GraspDescription(ApproachDirection.FRONT, VerticalAlignment.NoAlignment,
 #     ParkArmsActionDescription(arm=Arms.LEFT),
 #     MoveTorsoActionDescription(TorsoState.HIGH),
 # )
-
+VizMarkerPublisher()
 # Execute the plans
 with real_robot:
     SequentialPlan(

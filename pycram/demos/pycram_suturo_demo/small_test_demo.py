6f140ad28c2c193211fb23512f2328bb521d2722
Merckse <marc.kruse.2003@gmail.com>
2026-02-15 16:00:13 +0100

    fixes for small_demo after main changes

--- pycram/demos/pycram_suturo_demo/small_test_demo.py
+++ pycram/demos/pycram_suturo_demo/small_test_demo.py
@@ -1,11 +1,15 @@
+import rclpy
+
 from suturo_resources.suturo_map import load_environment
 
-from pycram.datastructures.enums import TorsoState, Arms
+from pycram.datastructures.enums import Arms
+from semantic_digital_twin.datastructures.definitions import TorsoState
 from pycram.language import SequentialPlan
 from pycram.process_module import simulated_robot
 from pycram.robot_plans import MoveTorsoActionDescription, ParkArmsActionDescription
 from simulation_setup import setup_hsrb_in_environment
 
+rclpy.init()
 result = setup_hsrb_in_environment(load_environment=load_environment, with_viz=True)
 world, robot_view, context, viz = (
     result.world,

db83e1d6c554d1f717d7a55b9a247ae2817c509e
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2026-05-28 15:44:31 +0200

    [InferenceExplanation] corrected negation handling for core logical operators

--- krrood/src/krrood/entity_query_language/operators/core_logical_operators.py
+++ krrood/src/krrood/entity_query_language/operators/core_logical_operators.py
@@ -92,7 +92,7 @@ class AND(LogicalBinaryOperator):
                 yield from self.evaluate_right(left_value)
         if not yielded:
             # Negation as failure: no variable value satisfied the condition. So the whole condition is False.
-            yield OperationResult(sources.bindings, False, self, sources)
+            yield OperationResult(sources.bindings, True, self, sources)
 
 
 @dataclass(eq=False, repr=False)

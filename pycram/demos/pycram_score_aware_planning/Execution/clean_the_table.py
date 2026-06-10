5a0605facf63aef5ff2d7def0da8b4d3b83b423b
makruse <makruse@uni-bremen.de>
2026-06-10 13:59:50 +0200

    changed coordinates of statically spawned object

--- pycram/demos/pycram_score_aware_planning/Execution/clean_the_table.py
+++ pycram/demos/pycram_score_aware_planning/Execution/clean_the_table.py
@@ -97,13 +97,13 @@ livingroom_tables = ["lowerTable", "dining_table", "shelf_1", "shelf_2"]
 office_tables = ["desk"]
 some_room = ["cooking_table"]
 
-generic_object_spawner(["Bowl"], [(1.325, 5.99, 0.81)], world, color=Color.GREEN())
+generic_object_spawner(["Bowl"], [(1.325, 6.23, 0.81)], world, color=Color.GREEN())
 generic_object_spawner(["Plate"], [(-0.15,0.88,0.85)], world, color=Color.ORANGE())
 generic_object_spawner(["Milk"], [(1.037,-2.31,0.645)], world, color=Color.RED())
-generic_object_spawner(["Knife"], [(4.65,4.84,1.62)], world, color=Color.GREEN())
-generic_object_spawner(["Apple"], [(4.135,1.865,0.54)], world, color=Color.GREEN())
-generic_object_spawner(["Cereal"], [(2.42,0.128,0.945)], world, color=Color.GREEN())
-generic_object_spawner(["Cup"], [(2.33475,5.215,0.83)], world, color=Color.GREEN())
+generic_object_spawner(["Knife"], [(4.65,4.84,1.62)], world, color=Color.CYAN())
+generic_object_spawner(["Apple"], [(4.135,1.865,0.54)], world, color=Color.WHITE())
+generic_object_spawner(["Cereal"], [(2.42,0.128,0.945)], world, color=Color.BLUE())
+generic_object_spawner(["Cup"], [(2.33475,5.215,0.83)], world, color=Color.BEIGE())
 
 manipulator = hsrb.arm.manipulator
 plan_parkarm = sequential([ParkArmsAction(Arms.LEFT)], context=context)
@@ -158,6 +158,7 @@ while task_plan != []:
             plan = sequential([], context=context)
             plan.add_child(make_node(action))
             result = plan.perform()
+            print(result)
 
 
 # if visible_bodies is None:

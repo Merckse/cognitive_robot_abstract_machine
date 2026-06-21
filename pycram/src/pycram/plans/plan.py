c4b66be6da56602429f6cd4106cbbf807c64b704
Merckse <marc.kruse.2003@gmail.com>
2026-06-21 17:30:33 +0200

    plan.py - fixed plan.remove_node() not reconnecting children, resulting in plan disconnecting and failing

--- pycram/src/pycram/plans/plan.py
+++ pycram/src/pycram/plans/plan.py
@@ -142,18 +142,23 @@ class Plan:
         self.remove_node(node2)
 
     def remove_node(self, node_for_removal: PlanNode):
-        """
-        Removes a node from the plan. If the node is not in the plan, it will be ignored.
+        if node_for_removal.plan is not self:
+            return
+        parent = node_for_removal.parent
+        children = node_for_removal.children
+        self.plan_graph.remove_node(node_for_removal.index)
+        node_for_removal.index = None
+        node_for_removal.layer_index = None
+        node_for_removal.plan = None
+        node_for_removal.world = None
+        if parent is not None:  # don't reparent if root
+            # reconnect children, so no accidental roots appear
+            for child in children:
+                self.add_edge(parent, child)
 
-        :param node_for_removal: Node to be removed
-        """
-        if node_for_removal.plan is self:
-            self.plan_graph.remove_node(node_for_removal.index)
-            node_for_removal.index = None
-            node_for_removal.layer_index = None
-            node_for_removal.plan = None
-            node_for_removal.world = None
-
+    def plan_graph_repair(self):
+        # TODO
+        pass
     def add_node(self, node: PlanNode):
         """
         Adds a node to the plan. The node will not be connected to any other node of the plan.
